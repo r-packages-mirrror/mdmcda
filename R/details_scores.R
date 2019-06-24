80829f1a101d5635ff554d3cd16ce3a0d1fba500
Gjalt-Jorn Peters <gjalt-jorn@behaviorchange.eu>
2019-06-24 21:48:12 +0200

    LateX output

--- R/details_scores.R
+++ R/details_scores.R
@@ -21,15 +21,29 @@ details_scores <- function(scenario_scores,
                     " {.tabset}\n\n",
                     "\n\n### Weighing profile: ",
                     j," {.tabset}\n\n");
-      res <-
-        paste0(res,
-               paste0(knitr::kable(scenario_scores$scenarioScores[[i]][[j]][,
-                                                                            c('decision_id',
-                                                                              'decision_alternative_value',
-                                                                              'criterion_id',
-                                                                              'score')],
-                                   row.names = FALSE),
-                      collapse="\n"));
+
+      if (knitr::is_latex_output()) {
+        res <-
+          paste0(res,
+                 paste0(knitr::kable(scenario_scores$scenarioScores[[i]][[j]][,
+                                                                              c('decision_id',
+                                                                                'decision_alternative_value',
+                                                                                'criterion_id',
+                                                                                'score')],
+                                     format="latex", booktabs = TRUE, longtable = TRUE,
+                                     row.names = FALSE),
+                        collapse="\n"));
+      } else {
+        res <-
+          paste0(res,
+                 paste0(knitr::kable(scenario_scores$scenarioScores[[i]][[j]][,
+                                                                              c('decision_id',
+                                                                                'decision_alternative_value',
+                                                                                'criterion_id',
+                                                                                'score')],
+                                     row.names = FALSE),
+                        collapse="\n"));
+      }
     }
   }
 
