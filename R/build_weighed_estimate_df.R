a8bd3df75b843799c091d7bf19ff1ef7a12d4071
Gjalt-Jorn Peters <gjalt-jorn@behaviorchange.eu>
2020-03-19 22:19:30 +0100

    Working towards single-decision functionality

--- R/build_weighed_estimate_df.R
+++ R/build_weighed_estimate_df.R
@@ -2,26 +2,41 @@
 build_weighed_estimate_df <-
   function(multiEstimateDf,
            criterionNames,
-           decisionNames,
-           scenarioNames,
-           scenarioDefinitions,
            scorer,
+           decisionNames = NULL,
+           scenarioNames = NULL,
+           scenarioDefinitions = NULL,
            setMissingEstimates = NULL,
            warnForMissingEstimates = TRUE,
            warnForDuplicateEstimates = TRUE) {
 
-    weighedEstimates <- data.frame(scenario_id=character(),
-                                   decision_id=character(),
-                                   alternative_value=numeric(),
-                                   criterion_id=character(),
-                                   estimate=numeric());
-    for (currentScenario in scenarioNames) {
-      for (currentDecision in decisionNames) {
+    if (is.null(scenarioNames) && is.null(scenarioDefinitions) && is.null(decisionNames)) {
+
+      currentDecision <- unique(estimates$multiEstimateDf$decision_id);
+
+      if (length(currentDecision) > 1) {
+        stop("When not working with scenarios, I can analyze only ",
+             "one decision, but the estimates your provided concern ",
+             length(unique(estimates$multiEstimateDf$decision_id)),
+             " decisions (specifically, ",
+             vecTxtQ(unique(estimates$multiEstimateDf$decision_id)), ").");
+      }
+
+      decision_alternative_values <-
+        unique(estimates$multiEstimateDf$decision_alternative_value);
+
+      weighedEstimates <- data.frame(scenario_id=character(),
+                                     decision_id=character(),
+                                     alternative_value=numeric(),
+                                     criterion_id=character(),
+                                     estimate=numeric());
+
+      for (currentAlternativeValue in decision_alternative_values) {
         for (currentCriterion in criterionNames) {
           estimate <-
             multiEstimateDf[multiEstimateDf$decision_id == currentDecision &
                               multiEstimateDf$decision_alternative_value ==
-                              scenarioDefinitions[[currentScenario]][currentDecision] &
+                              currentAlternativeValue &
                               multiEstimateDf$criterion_id==currentCriterion,
                             scorer];
           if (is.null(estimate) || is.na(estimate) || (length(estimate) == 0)) {
@@ -29,7 +44,7 @@ build_weighed_estimate_df <-
                 (length(setMissingEstimates) == 1)) {
               if (warnForMissingEstimates) {
                 cat("\n- Warning: no estimate found for the effect of alternative ",
-                    scenarioDefinitions[[currentScenario]][currentDecision],
+                    currentAlternativeValue,
                     " of decision ", currentDecision, " on criterion ",
                     currentCriterion, " - setting it to the value of `setMissingEstimates` (",
                     setMissingEstimates, ")\n", sep="");
@@ -48,7 +63,7 @@ build_weighed_estimate_df <-
           if (length(estimate) > 1) {
             if (warnForDuplicateEstimates) {
               cat("\n- Warning: multiple estimates found for the effect of alternative ",
-                  scenarioDefinitions[[currentScenario]][currentDecision],
+                  currentAlternativeValue,
                   " of decision ", currentDecision, " on criterion ",
                   currentCriterion, ". Averaging them.\n", sep="");
             }
@@ -56,9 +71,9 @@ build_weighed_estimate_df <-
           }
           weighedEstimates <-
             rbind(weighedEstimates,
-                  data.frame(scenario_id=currentScenario,
+                  data.frame(scenario_id="none",
                              decision_id=currentDecision,
-                             alternative_value=scenarioDefinitions[[currentScenario]][currentDecision],
+                             alternative_value=currentAlternativeValue,
                              criterion_id=currentCriterion,
                              estimate=estimate,
                              stringsAsFactors = FALSE));
@@ -67,7 +82,77 @@ build_weighed_estimate_df <-
           #     ", found estimate ", estimate, ".", sep="");
         }
       }
+
+    } else if (!is.null(scenarioNames) &&
+               !is.null(scenarioDefinitions) &&
+               !is.null(decisionNames)) {
+
+      weighedEstimates <- data.frame(scenario_id=character(),
+                                     decision_id=character(),
+                                     alternative_value=numeric(),
+                                     criterion_id=character(),
+                                     estimate=numeric());
+      for (currentScenario in scenarioNames) {
+        for (currentDecision in decisionNames) {
+          for (currentCriterion in criterionNames) {
+            estimate <-
+              multiEstimateDf[multiEstimateDf$decision_id == currentDecision &
+                                multiEstimateDf$decision_alternative_value ==
+                                scenarioDefinitions[[currentScenario]][currentDecision] &
+                                multiEstimateDf$criterion_id==currentCriterion,
+                              scorer];
+            if (is.null(estimate) || is.na(estimate) || (length(estimate) == 0)) {
+              if (!is.null(setMissingEstimates) & is.numeric(setMissingEstimates) &
+                  (length(setMissingEstimates) == 1)) {
+                if (warnForMissingEstimates) {
+                  cat("\n- Warning: no estimate found for the effect of alternative ",
+                      scenarioDefinitions[[currentScenario]][currentDecision],
+                      " of decision ", currentDecision, " on criterion ",
+                      currentCriterion, " - setting it to the value of `setMissingEstimates` (",
+                      setMissingEstimates, ")\n", sep="");
+                }
+                estimate <- setMissingEstimates;
+              } else {
+                if (warnForMissingEstimates) {
+                  cat("\n- Error: no estimate found for the effect of alternative ",
+                      scenarioDefinitions[[currentScenario]][currentDecision],
+                      " of decision ", currentDecision, " on criterion ",
+                      currentCriterion, ".\n", sep="");
+                }
+                estimate <- NA;
+              }
+            }
+            if (length(estimate) > 1) {
+              if (warnForDuplicateEstimates) {
+                cat("\n- Warning: multiple estimates found for the effect of alternative ",
+                    scenarioDefinitions[[currentScenario]][currentDecision],
+                    " of decision ", currentDecision, " on criterion ",
+                    currentCriterion, ". Averaging them.\n", sep="");
+              }
+              estimate <- mean(estimate, na.rm = TRUE);
+            }
+            weighedEstimates <-
+              rbind(weighedEstimates,
+                    data.frame(scenario_id=currentScenario,
+                               decision_id=currentDecision,
+                               alternative_value=scenarioDefinitions[[currentScenario]][currentDecision],
+                               criterion_id=currentCriterion,
+                               estimate=estimate,
+                               stringsAsFactors = FALSE));
+            # cat("\nWithin scenario ", currentScenario, ", for the effect of decision ",
+            #     currentDecision, " for criterion ", currentCriterion,
+            #     ", found estimate ", estimate, ".", sep="");
+          }
+        }
+      }
+    } else {
+      stop("When working with scenarios, all three arguments ",
+          "`decisionNames`, `scenarioNames`, and `scenarioDefinitions` ",
+          "must be provided; when not working with scenarios (and ",
+          "sticking to one decision), none of these three must be provided!");
     }
+
     return(weighedEstimates);
+
   }
 
